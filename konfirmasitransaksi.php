83c45295e7b7c62abd3cc2f606556c3195e7bcef
Diksi Erlangga <diksi131@gmail.com>
2019-01-30 21:01:07 +0800

    ubah message

--- konfirmasitransaksi.php
+++ konfirmasitransaksi.php
@@ -3,7 +3,7 @@ include 'database/config.php';
 
 $ID=$_GET['ID'];
 
-$sql = "UPDATE tbtransaksi SET status_transaksi=('Di Keuangan'), tgl_konf=NOW() WHERE id_transaksi='$ID';";
+$sql = "UPDATE tbtransaksi SET status_transaksi=('Di Supplier'), tgl_konf=NOW() WHERE id_transaksi='$ID';";
 
 mysqli_query($conn, $sql);
 
